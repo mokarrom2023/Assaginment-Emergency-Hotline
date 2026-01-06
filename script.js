9e3c33877e8a341687cc82580f1c8c5173be7bd1
ProgrammingHero1 <mokarromislam.2023@gmail.com>
2026-01-07 00:47:39 +0600

    add script.js file card information

---
+++ script.js
@@ -0,0 +1,23 @@
+const services = [
+    { id: 1, name: "National Emergency Number", nameEn: "National Emergency", number: "999", image: "assets/emergency.png", category: "All" },
+
+    { id: 2, name: "Police Helpline Number", nameEn: "Police", number: "999", image: "assets/police.png", category: "Police" },
+
+    { id: 3, name: "Fire Service Number", nameEn: "Fire Service", number: "999", image: "assets/fire-service.png", category: "Fire" },
+
+    { id: 4, name: "Ambulance Service", nameEn: "Ambulance", number: "1994-999999", image: "assets/ambulance.png", category: "Health" },
+
+    { id: 5, name: "Women & Child Helpline", nameEn: "Women & Child Helpline", number: "109", image: "assets/emergency.png", category: "Help" },
+
+    { id: 6, name: "Anti-Corruption Helpline", nameEn: "Anti-Corruption", number: "106", image: "assets/emergency.png", category: "Govt." },
+
+    { id: 7, name: "Electricity Helpline", nameEn: "Electricity Outage", number: "16216", image: "assets/emergency.png", category: "Electricity" },
+
+    { id: 8, name: "Brac Helpline", nameEn: "Brac", number: "16445", image: "assets/brac.png", category: "NGO" },
+
+    { id: 9, name: "Bangladesh Railway Helpline ", nameEn: "Bangladesh Railway", number: "163", image: "assets/Bangladesh-Railway.png", category: "Travel" }
+];
+
+let coins = 100,
+ hearts = 0,
+ copies = 0;
